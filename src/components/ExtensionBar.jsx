cd26fd87249af66e0b4a8fa0edf57892eada02d4
s.zerradi <salmane.zerradi@de2s.com>
2025-04-14 20:29:50 +0100

    change bg color of extensions bar

--- src/components/ExtensionBar.jsx
+++ src/components/ExtensionBar.jsx
@@ -9,7 +9,7 @@ import { useTheme } from '../contexts/ThemeContext';
 const ExtensionBar = () => {
     const  { darkMode, toggleDarkMode } = useTheme();
   return (
-    <div className="flex justify-between max-w-7xl mx-auto p-3 sm:px-3 lg:px-4 bg-neutral-0 dark:bg-neutral-600 rounded-xl ">
+    <div className="flex justify-between max-w-7xl mx-auto p-3 sm:px-3 lg:px-4 bg-neutral-0 dark:bg-neutral-800 rounded-xl ">
       <img src={darkMode? logoDarkMpdeSrc : logoSrc}/>
       <button onClick={toggleDarkMode}>
       {darkMode? 
